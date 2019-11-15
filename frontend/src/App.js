ccd874e2a195e141cf3e0503e2ecce192922f394
David Hacker <dmhacker@protonmail.com>
2019-11-15 12:47:18 -0800

    Start refractoring frontend to separate components, make simpler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
-import Home from './components/home';
-import Channel from './components/channel';
+import Home from './pages/home';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Route path="/" exact render={(props) => <Home {...props}/>}/>
-        <Route path="/channel/:name" render={(props) => <Channel {...props}/>}/>
       </div>
     ); 
   }
